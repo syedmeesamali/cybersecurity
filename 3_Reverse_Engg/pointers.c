9811451290f5544f3b1b095a4dde07638393b3a4
syedmeesamali <syedmeesamali@yahoo.com>
2024-02-09 23:22:10 +0300

    Double pointers and arrays in C - Ubuntu on VM

---
+++ 3_Reverse_Engg/pointers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void SetToZero(short **);         //Double pointers
+int main()
+{
+    short a[3] = {15, 28, 35};
+    short b[2] = {18, 29};
+    short *t[2] = {a, b};   //Array of addresses of arrays
+    printf("Current array a = %d, %d, %d\n", a[0], a[1], a[2]);
+    printf("Current array b = %d, %d\n", b[0], b[1]);
+    SetToZero(t);
+    printf("New array a = %d, %d, %d\n", a[0], a[1], a[2]);
+    printf("New array b = %d, %d\n", b[0], b[1]);
+
+    return 0;
+}
+
+void SetToZero(short **val)
+{
+    printf("-------------------\n");
+    *(*val) = 0;            //First element of first array
+    *(*(val)+1) = 0;        //Pointer arithmetic - second element of first array
+    *(*(val)+2) = 0;        //3rd element of first array
+}
