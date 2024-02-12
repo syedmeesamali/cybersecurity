034779cf6379f40c9f2e31462e25d0a5b3113755
syedmeesamali <syedmeesamali@yahoo.com>
2024-02-12 23:07:20 +0300

    Double arrays study and various methods of updates - Ubuntu

--- 3_Reverse_Engg/pointers.c
+++ 3_Reverse_Engg/pointers.c
@@ -10,17 +10,19 @@ int main()
     SetToZero(t);
     printf("New array a = %d, %d, %d\n", a[0], a[1], a[2]);
     printf("New array b = %d, %d\n", b[0], b[1]);
-
     return 0;
 }
 
+//Make elements of the array zero
 void SetToZero(short **val)
 {
     printf("-------------------\n");
-    *(*val) = 0;            //First element of first array
-    *(*(val)+1) = 0;        //Pointer arithmetic - second element of first array
-    *(*(val)+2) = 0;        //3rd element of first array
+    *(*val) = 0;            //First element of first array t[0][0]
+    val[0][0] = 1;
+    *(*(val)+1) = 0;        //Pointer arithmetic - second element of first array t[0][1]
+    *(val[0] + 1) = 2;
+    *(*(val)+2) = 0;        //3rd element of first array t[0][2]
 
-    *(*(val+1)) = 0;
-    *(*(val+1)+1) = 0;
-}
+    *(*(val+1)) = 0;        //t[1][0]
+    *(*(val+1)+1) = 0;      //t[1][1]
+} //End of make array elements zero function
